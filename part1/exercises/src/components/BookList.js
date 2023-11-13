ac47a9ace6bd3112fb6a2c75716379f4b5f713f6
Dia <142447820+marksofdespair@users.noreply.github.com>
2023-11-13 11:04:22 -0600

    exercise p1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Good Reads!";
+   let book1 = "https://m.media-amazon.com/images/I/71gNzw663DL._AC_UF1000,1000_QL80_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/71-8PDDqdkL._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/81X16F1lH-L._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Warrior Cats: Into The Wild, series by Erin Hunter" />
+         <img src={book2} alt="Warrior Cats: A New Prophecy, series by Erin Hunter" />
+         <img src={book3} alt="Warrior Cats: Power of Three, series by Erin Hunter" />
       </div>      
    );
 }
