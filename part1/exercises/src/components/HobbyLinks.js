f175902465f75944587c0630538fe9ea3cc625c6
Dia <142447820+marksofdespair@users.noreply.github.com>
2023-11-13 11:09:39 -0600

    forgot hobby links

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,9 @@
+export default function HobbyLinks () {
+    let hobbyLinks = ['https://www.leagueoflegends.com/en-us/', 'https://act.hoyoverse.com/puzzle/hk4e/pz_DvjBejelna/index.html'];
+    return (
+    <div>
+        <a href = {hobbyLinks[0]}>League of Legends</a>
+        <a href = {hobbyLinks[1]}>Genshin Impact</a>
+    </div>
+    )
+ }
