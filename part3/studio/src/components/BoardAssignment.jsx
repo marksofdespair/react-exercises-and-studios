4810746c0ee3e9282aecfc16b4ad5d891e1a8b04
Dia <142447820+marksofdespair@users.noreply.github.com>
2023-11-20 19:53:53 -0600

    semifunctional studio maybe

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,18 +1,39 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const boards = [
+      {
+      label: "Sweet Recipes",
+      value: "Put Sweet Recipes Here!"
+      },
+      {
+      label: "Savory Recipes",
+      value: "Put Savory Recipes Here!"
+      },
+      {
+      label: "Spicy Recipes",
+      value: "Put Spicy Recipes Here!"
+      }
+      ];
 
+   const [boardName, setBoardName] = useState('no boards yet!');
+   
    const handleChange = (event) => {
+      setBoardName(event.target.value);
    }
 
    return (
-      <div style={{paddingTop: "50px"}}>
-      <label>Save to Board: </label>
-      <select value={boardName} onChange={handleChange}>
-      </select>
-
-      <p>Saved to {boardName}!</p>
+      <div style={{ paddingTop: "50px" }}>
+        <label>Save to Board: </label>
+        <select value={boardName} onChange={handleChange}>
+          {boards.map((board) => (
+            <option key={board.label} value={board.label}>
+              {board.label}
+            </option>
+          ))}
+        </select>
+  
+        <p>Saved to {boardName}!</p>
       </div>
    );
 }
